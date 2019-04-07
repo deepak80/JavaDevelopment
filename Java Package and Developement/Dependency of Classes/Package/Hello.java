96de4333b27cc70deef498c4200674a7d4dcd1e2
Deepak Patre <47783824+Hello-DeepakPatre@users.noreply.github.com>
2019-04-07 10:57:11 +0530

    Practice with Packages
    Multi-level packages.

---
+++ Java Package and Developement/Dependency of Classes/Package/Hello.java
@@ -0,0 +1,6 @@
+package com.pack1.pack2;
+public class Hello {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
---
+++ Java Package and Developement/Dependency of Classes/Package/Inherit.java
@@ -0,0 +1,11 @@
+package com.pack3.pack4;
+public class Inherit {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
+class Child extends Inherit {
+	public static void main(String[] args){
+		System.out.println("Child class.");
+	}
+}
